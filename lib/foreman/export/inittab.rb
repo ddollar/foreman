314e2f553030b4fe9001ce5a1c6722b851f2b95c
David Dollar <ddollar@gmail.com>
2010-06-29 17:08:30 -0400

    fix -l, dont append app name

--- lib/foreman/export/inittab.rb
+++ lib/foreman/export/inittab.rb
@@ -7,23 +7,21 @@ class Foreman::Export::Inittab < Foreman::Export::Base
     user = options[:user] || app
     log_root = options[:log] || "/var/log"
 
-    log_dir = "#{log_root}/#{app}"
-
     concurrency = parse_concurrency(options[:concurrency])
 
     inittab = []
     inittab << "# ----- foreman #{app} processes -----"
     engine.processes.values.each_with_index do |process, num|
       id = app.slice(0, 2).upcase + sprintf("%02d", num+1)
-      inittab << "#{id}:4:respawn:/bin/su - #{user} -c '#{process.command} >> #{log_dir}/#{process.name}-#{num+1}.log 2>&1'"
+      inittab << "#{id}:4:respawn:/bin/su - #{user} -c '#{process.command} >> #{log_root}/#{process.name}-#{num+1}.log 2>&1'"
     end
     inittab << "# ----- end foreman #{app} processes -----"
 
     inittab = inittab.join("\n") + "\n"
 
     if fname
-      FileUtils.mkdir_p(log_dir)
-      FileUtils.chown(user, nil, log_dir)
+      FileUtils.mkdir_p(log_root)
+      FileUtils.chown(user, nil, log_root)
       write_file(fname, inittab)
     else
       puts inittab
