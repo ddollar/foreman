577a5c7c5c7eaa850cde7c28d445653f4641012c
David Dollar <ddollar@gmail.com>
2011-06-02 12:09:30 -0400

    make sure tests run on machines other than mine

--- spec/foreman/export/upstart_spec.rb
+++ spec/foreman/export/upstart_spec.rb
@@ -1,9 +1,11 @@
 require "spec_helper"
 require "foreman/engine"
 require "foreman/export/upstart"
+require "tmpdir"
 
 describe Foreman::Export::Upstart do
-  let(:engine) { Foreman::Engine.new(write_procfile) }
+  let(:procfile) { FileUtils.mkdir_p("/tmp/app"); write_procfile("/tmp/app/Procfile") }
+  let(:engine) { Foreman::Engine.new(procfile) }
   let(:upstart) { Foreman::Export::Upstart.new(engine) }
 
   before(:each) { load_export_templates_into_fakefs("upstart") }
@@ -11,12 +13,13 @@ describe Foreman::Export::Upstart do
 
   it "exports to the filesystem" do
     upstart.export("/tmp/init")
+    p [:d, Dir["/tmp/init/**"]]
 
-    File.read("/tmp/init/foreman.conf").should         == example_export_file("upstart/foreman.conf")
-    File.read("/tmp/init/foreman-alpha.conf").should   == example_export_file("upstart/foreman-alpha.conf")
-    File.read("/tmp/init/foreman-alpha-1.conf").should == example_export_file("upstart/foreman-alpha-1.conf")
-    File.read("/tmp/init/foreman-alpha-2.conf").should == example_export_file("upstart/foreman-alpha-2.conf")
-    File.read("/tmp/init/foreman-bravo.conf").should   == example_export_file("upstart/foreman.bravo.conf")
-    File.read("/tmp/init/foreman-bravo-1.conf").should == example_export_file("upstart/foreman-bravo-1.conf")
+    File.read("/tmp/init/app.conf").should         == example_export_file("upstart/app.conf")
+    File.read("/tmp/init/app-alpha.conf").should   == example_export_file("upstart/app-alpha.conf")
+    File.read("/tmp/init/app-alpha-1.conf").should == example_export_file("upstart/app-alpha-1.conf")
+    File.read("/tmp/init/app-alpha-2.conf").should == example_export_file("upstart/app-alpha-2.conf")
+    File.read("/tmp/init/app-bravo.conf").should   == example_export_file("upstart/app-bravo.conf")
+    File.read("/tmp/init/app-bravo-1.conf").should == example_export_file("upstart/app-bravo-1.conf")
   end
 end
