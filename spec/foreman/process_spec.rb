2ed1fe8d44f2007bcade24ff613ff20933af0c40
brainopia <brainopia@evilmartians.com>
2012-01-22 08:54:42 +0400

    Add specs for initialization of Foreman::Process

--- spec/foreman/process_spec.rb
+++ spec/foreman/process_spec.rb
@@ -1,2 +1,19 @@
 require "spec_helper"
 require "foreman/process"
+require 'ostruct'
+
+describe Foreman::Process do
+  subject { described_class.new entry, number, port }
+
+  let(:number)  { 1 }
+  let(:port)    { 777 }
+  let(:command) { :script }
+  let(:name)    { :foobar }
+  let(:entry)   { OpenStruct.new :name => name, :command => command }
+
+  its(:entry) { entry }
+  its(:num)   { number }
+  its(:port)  { port }
+  its(:name)  { "#{name}.#{port}" }
+  its(:pid)   { nil }
+end
