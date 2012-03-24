bc03d6a77a8ac8495ea7731a6c6b8f6cc88b8f83
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-24 10:50:44 -0700

    Spec for monitoring options for 'export' (for monit)

--- spec/foreman/export/monit_spec.rb
+++ spec/foreman/export/monit_spec.rb
@@ -16,7 +16,7 @@ describe Foreman::Export::Monit, :fakefs do
   it "exports to the filesystem" do
     monit.export
 
-    File.read("/tmp/init/app.monitrc").should         == example_export_file("monit/app-alpha1-bravo1.monitrc")
+    File.read("/tmp/init/app.monitrc").should == example_export_file("monit/app-alpha1-bravo1.monitrc")
   end
 
   it "cleans up if exporting into an existing dir" do
@@ -32,7 +32,23 @@ describe Foreman::Export::Monit, :fakefs do
     it "exports to the filesystem with concurrency" do
       monit.export
 
-      File.read("/tmp/init/app.monitrc").should         == example_export_file("monit/app-alpha2.monitrc")
+      File.read("/tmp/init/app.monitrc").should == example_export_file("monit/app-alpha2.monitrc")
+    end
+  end
+
+  context "with concurrency and process monitoring" do
+    let(:options) do {
+      :concurrency      => "alpha=2,bravo=1",
+      :"restart-on-mem" => "alpha=157 MB,bravo=300 MB", # explicit processes
+      :"restart-on-cpu" => "alpha=25%",                 # specific processes
+      :"alert-on-cpu"   => "22%"                        # all
+    }
+    end
+
+    it "exports to the filesystem with concurrency and cpu/mem monitoring" do
+      monit.export
+
+      File.read("/tmp/init/app.monitrc").should == example_export_file("monit/app-alpha2-bravo1-monitoring.monitrc")
     end
   end
 
