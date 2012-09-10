c617ddb3b2ea74616b40227dbcb2490ebfa46f73
Aaron Kalin <akalin@martinisoftware.com>
2012-09-10 18:21:51 -0500

    Cleanup launchd exporter
    Also suppresses the deprecation warning for the launchd exporter when
    using port instead of engine.port_for

--- lib/foreman/export/launchd.rb
+++ lib/foreman/export/launchd.rb
@@ -7,6 +7,7 @@ class Foreman::Export::Launchd < Foreman::Export::Base
     super
     engine.each_process do |name, process|
       1.upto(engine.formation[name]) do |num|
+        port = engine.port_for(process, num)
         write_template "launchd/launchd.plist.erb", "#{app}-#{name}-#{num}.plist", binding
       end
     end
