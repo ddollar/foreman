698e6ae092112e6b7404814ca8ecfbba14b42087
Aaron Kalin <akalin@martinisoftware.com>
2012-09-10 19:10:47 -0500

    Add command argument string splitting
    launchd uses exec() under the hood when you specify ProgramArguments
    or Program so you must break apart program arguments into separate
    strings in an array or the command will fail

--- lib/foreman/export/launchd.rb
+++ lib/foreman/export/launchd.rb
@@ -8,6 +8,7 @@ class Foreman::Export::Launchd < Foreman::Export::Base
     engine.each_process do |name, process|
       1.upto(engine.formation[name]) do |num|
         port = engine.port_for(process, num)
+        command_args = process.command.split(" ")
         write_template "launchd/launchd.plist.erb", "#{app}-#{name}-#{num}.plist", binding
       end
     end
