55fd6e6c071b3e2ed439ab1b40a3426e19b9135c
Victor Brender <victor@2xwide.com>
2015-03-06 16:18:30 +0100

    Vendored ‘dotenv’ gem.

---
+++ vendor/dotenv-1.0.2/lib/dotenv/substitutions/command.rb
@@ -0,0 +1,39 @@
+module Dotenv
+  module Substitutions
+    # Substitute shell commands in a value.
+    #
+    #   SHA=$(git rev-parse HEAD)
+    #
+    module Command
+      class << self
+
+        INTERPOLATED_SHELL_COMMAND = /
+          (?<backslash>\\)?   # is it escaped with a backslash?
+          \$                  # literal $
+          (?<cmd>             # collect command content for eval
+            \(                # require opening paren
+            ([^()]|\g<cmd>)+  # allow any number of non-parens, or balanced parens (by nesting the <cmd> expression recursively)
+            \)                # require closing paren
+          )
+        /x
+
+        def call(value, env)
+          # Process interpolated shell commands
+          value.gsub(INTERPOLATED_SHELL_COMMAND) do |*|
+            # Eliminate opening and closing parentheses
+            command = $~[:cmd][1..-2]
+
+            if $~[:backslash]
+              # Command is escaped, don't replace it.
+              $~[0][1..-1]
+            else
+              # Execute the command and return the value
+              `#{command}`.chomp
+            end
+          end
+        end
+      end
+
+    end
+  end
+end
