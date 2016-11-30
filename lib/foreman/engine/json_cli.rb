b4916814f4c78e48cd0dc2aa28f1a69f4b29ccf6
Matt Pestritto <matt@pestritto.com>
2016-11-30 11:39:12 -0500

    change date time format to incude microseconds

--- lib/foreman/engine/json_cli.rb
+++ lib/foreman/engine/json_cli.rb
@@ -9,12 +9,12 @@ class Foreman::Engine::JsonCLI < Foreman::Engine
   def output(name, data)
     data.to_s.lines.map(&:chomp).each do |message|
       begin
-        json_data = JSON.parse(message).to_s
+        json_data = JSON.parse(message).to_json
       rescue JSON::ParserError
         json_data = { line: message }.to_json
       end
       json_data.insert(1, "\"process_name\":\"#{name}\", ")
-      json_data.insert(1, "\"application_server_timestamp\":\"#{Time.now.utc.to_s}\", ")
+      json_data.insert(1, "\"application_server_timestamp\":\"#{Time.now.strftime("%Y-%m-%d %H:%M:%S.%6N %z")}\", ")
       output = json_data
       $stdout.puts output
     end
