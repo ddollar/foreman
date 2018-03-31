b25eca38137668fbc4a39dd1297e54fa232c5993
William Fish <william.michael.fish@gmail.com>
2018-03-31 01:04:44 +0100

    Small non-functional refactor

--- lib/foreman/env.rb
+++ lib/foreman/env.rb
@@ -1,22 +1,20 @@
 require "foreman"
 
+# Reads and exposes environment variables
+# from a .env file.
 class Foreman::Env
-
-  attr_reader :entries
-
   def initialize(filename)
-    @entries = File.read(filename).gsub("\r\n","\n").split("\n").inject({}) do |ax, line|
-      if line =~ /\A([A-Za-z_0-9]+)=(.*)\z/
-        key = $1
-        case val = $2
+    @entries = File.read(filename).gsub("\r\n", "\n").split("\n").each_with_object({}) do |line, ax|
+      next unless line =~ /\A([A-Za-z_0-9]+)=(.*)\z/
+      key = Regexp.last_match(1)
+      ax[key] =
+        case val = Regexp.last_match(2)
           # Remove single quotes
-          when /\A'(.*)'\z/ then ax[key] = $1
+        when /\A'(.*)'\z/ then Regexp.last_match(1)
           # Remove double quotes and unescape string preserving newline characters
-          when /\A"(.*)"\z/ then ax[key] = $1.gsub('\n', "\n").gsub(/\\(.)/, '\1')
-         else ax[key] = val
+        when /\A"(.*)"\z/ then Regexp.last_match(1).gsub('\n', "\n").gsub(/\\(.)/, '\1')
+        else val
         end
-      end
-      ax
     end
   end
 
@@ -25,5 +23,4 @@ class Foreman::Env
       yield key, value
     end
   end
-
 end
