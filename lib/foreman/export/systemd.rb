582a38f04dfc7136fcb970c412080c06dbdffb71
Robin Daugherty <robin@robindaugherty.net>
2019-12-10 10:44:07 -0500

    systemd sets process name
    No longer using systemd templates because it's not possible to set multiple variables in each. Now $PS is set just like it is when running foreman.

--- lib/foreman/export/systemd.rb
+++ lib/foreman/export/systemd.rb
@@ -17,22 +17,16 @@ class Foreman::Export::Systemd < Foreman::Export::Base
       clean_dir file
     end
 
-    process_master_names = []
+    service_names = []
 
     engine.each_process do |name, process|
-      service_fn = "#{app}-#{name}@.service"
-      write_template "systemd/process.service.erb", service_fn, binding
-
-      create_directory("#{app}-#{name}.target.wants")
-      1.upto(engine.formation[name])
-        .collect { |num| engine.port_for(process, num) }
-        .collect { |port| "#{app}-#{name}@#{port}.service" }
-        .each do |process_name|
-        create_symlink("#{app}-#{name}.target.wants/#{process_name}", "../#{service_fn}") rescue Errno::EEXIST # This is needed because rr-mocks do not call the origial cleanup
+      1.upto(engine.formation[name]) do |num|
+        port = engine.port_for(process, num)
+        process_name = "#{name}.#{num}"
+        service_filename = "#{app}-#{process_name}.service"
+        write_template "systemd/process.service.erb", service_filename, binding
+        service_names << service_filename
       end
-
-      write_template "systemd/process_master.target.erb", "#{app}-#{name}.target", binding
-      process_master_names << "#{app}-#{name}.target"
     end
 
     write_template "systemd/master.target.erb", "#{app}.target", binding
